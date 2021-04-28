4be6a0ecb5de1565beff96a30f48c9cdca06e1e3
Krish Goel <krishgoel3@gmail.com>
2021-04-28 16:33:06 +0530

    -

--- src/routes/Volunteer.svelte
+++ src/routes/Volunteer.svelte
@@ -11,7 +11,7 @@
         <h1>Vounteer with us</h1>
         <br/>
         <p>
-            CovAID is the joint initiative of 5 Sanskriti School students. We have initiated a unified online platform for smooth and accurate dispersal of information regarding availability of Covid-19 resources in the National Capital Region. We will be posting verified updates on the platform - checking the updated availability by calling hospitals and drug sellers at fixed intervals. 
+            CovAID is the joint initiative of 5 High School students. We have initiated a unified online platform for smooth and accurate dispersal of information regarding availability of Covid-19 resources in the National Capital Region. We will be posting verified updates on the platform - checking the updated availability by calling hospitals and drug sellers at fixed intervals. 
             <br/><br/>
             For our initiative to maximise its reach and resourcefulness, <strong>we need a bigger working force, which can regularly update the data</strong>. They will be allotted time slots of upto 2 hours, in which they have to call the assigned hospitals and suppliers. <strong>All volunteers will be acknowledged via verifiable certificates</strong>.
             <br/><br/>
